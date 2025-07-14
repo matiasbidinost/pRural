cff3e3b4e1eaa06590213d9ceac8f9a9d06495d4
matiasbidinost <112261479+matiasbidinost@users.noreply.github.com>
2025-07-14 09:39:22 -0300

    first change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/auth/login/login').then(m => m.Login)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./components/auth/login/login').then(m => m.Login)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./components/auth/register/register').then(m => m.Register)
+    }];
+    
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
